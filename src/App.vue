0c49b8df1781b531669dd0f42162098f02154ff3
Bólya Gábor <bolya.gabor@gmail.com>
2022-09-03 07:41:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import NameList from './components/NameList.vue'
+import InputForm from './components/InputForm.vue'
+
+import { ref } from "@vue/reactivity";
+
+let names = ref(["józsi", "Béla"]);
+//console.log(names.value[0]);
+
+const addToList = (data)=>{
+  names.value.push(data)
+ }
+
+</script>
+
+<template>
+  <input-form @addToList="addToList"/>
+
+  <name-list :names="names"/>
+  <!-- {{ names }} -->
+
+  
+</template>
+
+<style scoped>
+</style>
